5935e15f4a89b321170adb9f568469ac2b9f79e7
4M <mossmost123@gmail.com>
2023-05-03 22:09:09 +0700

    FirstStep

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,17 @@
 package Main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+	
+	public void start(Stage stage) {
+		stage.setTitle("Finding For Name???");
+		
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+        launch();
+    }
 }
